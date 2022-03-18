e8739861bb22f279273b664bb00f39c3204ba0b1
infamous55 <infamouslegend555@gmail.com>
2022-03-18 19:04:05 +0200

    prisma seed

---
+++ prisma/seed.js
@@ -0,0 +1,24 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.upsert({
+    where: { email: 'admin@admin.com' },
+    update: {},
+    create: {
+      email: 'admin@admin.com',
+      name: 'admin',
+      password: 'password',
+      role: 'ADMIN',
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
