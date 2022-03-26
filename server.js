26f6f9b8c24558f4853817381566045be979d1f9
infamous55 <infamouslegend555@gmail.com>
2022-03-26 18:13:28 +0200

    basic security

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const app = express();
 
 require('dotenv').config();
 
 app.use(
-  bodyParser.urlencoded({
+  express.urlencoded({
     extended: true,
   })
 );
-app.use(bodyParser.json());
+app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 else app.use(morgan('combined'));
 
-app.use(require('./routes'));
+app.use(cors());
+app.use(helmet());
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use('/api/v1', apiLimiter, require('./routes'));
 
 const PORT = 5000 || process.env.PORT;
 
